c47632cec57e00516e7d4be207cf661edf539e70
Vladimir Gavrylov <iboved@gmail.com>
2014-11-27 12:22:47 +0200

    Change RandomController

--- src/Iboved/TestBundle/Controller/RandomController.php
+++ src/Iboved/TestBundle/Controller/RandomController.php
@@ -6,28 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class RandomController extends Controller
 {
-    public function indexAction()
+    public function indexAction($limit)
     {
-        $number = rand(1, 5);
+        $number = rand(1, $limit);
 
-        switch($number) {
-            case 1:
-                $result = 'Default:sun';
-                break;
-            case 2:
-                $result = 'Default:mercury';
-                break;
-            case 3:
-                $result = 'Default:venus';
-                break;
-            case 4:
-                $result = 'Default:earth';
-                break;
-            case 5:
-                $result = 'Default:mars';
-                break;
-        }
-
-        return $this->render("IbovedTestBundle:{$result}.html.twig");
+        return $this->render('IbovedTestBundle:Random:index.html.twig', array('number' => $number));
     }
 }
