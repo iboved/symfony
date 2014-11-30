d8ffcbb9dd1add1608b85c093d46445c6996778e
Vladimir Gavrylov <iboved@gmail.com>
2014-11-30 15:53:48 +0200

    Change DefaultController

--- src/Iboved/StoreBundle/Controller/DefaultController.php
+++ src/Iboved/StoreBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace Iboved\StoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Iboved\StoreBundle\Entity\Product;
+use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
@@ -10,8 +12,17 @@ class DefaultController extends Controller
     /**
      * @Route("/")
      */
-    public function indexAction()
+    public function createAction()
     {
-        return $this->render('IbovedStoreBundle:Default:index.html.twig');
+        $product = new Product();
+        $product->setName('A Foo Bar');
+        $product->setPrice('19.99');
+        $product->setDescription('Lorem ipsum dolor');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
+
+        return new Response('Created product id '.$product->getId());
     }
 }
